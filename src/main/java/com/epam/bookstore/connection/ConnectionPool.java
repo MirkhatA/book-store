cd12f5662678866bd99293cf118f53ad69a72e71
Mirkhat Asen <asen.mirkhat@bk.ru>
2022-06-02 22:38:11 +0600

    Connection Pool

--- src/main/java/com/epam/bookstore/connection/ConnectionPool.java
+++ src/main/java/com/epam/bookstore/connection/ConnectionPool.java
@@ -1,6 +1,12 @@
 package com.epam.bookstore.connection;
 
+import java.sql.*;
+import java.util.HashSet;
 import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.Stack;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public final class ConnectionPool {
 
@@ -8,14 +14,83 @@ public final class ConnectionPool {
     private String dbUser;
     private String dbPass;
     private String dbDriver;
+
     private ResourceBundle properties = ResourceBundle.getBundle("database");
-    private int maxPoolSize = 5;
+    private final int maxPoolSize = 10;
+    private static volatile ConnectionPool instance;
+    private BlockingQueue<Connection> connectionQueue = new ArrayBlockingQueue<>(maxPoolSize);
+
+    private ConnectionPool() {
+        init();
+    }
+
+    private void init() {
+        setConnectionData();
+        loadDrivers();
+        initPoolData();
+    }
 
     private void setConnectionData() {
         this.dbUrl = properties.getString("dbUrl");
         this.dbUser = properties.getString("dbUser");
         this.dbPass = properties.getString("dbPass");
         this.dbDriver = properties.getString("dbDriver");
+    }
+
+    private void loadDrivers() {
+        try {
+            Driver driver = (Driver) Class.forName(dbDriver).newInstance();
+        } catch (InstantiationException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ConnectionPool getInstance() {
+        ConnectionPool localInstance = instance;
+        if (localInstance == null) {
+            synchronized (ConnectionPool.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new ConnectionPool();
+                }
+            }
+        }
+        return localInstance;
+    }
+
+    private void initPoolData() {
+        Connection connection;
+
+        while (connectionQueue.size() < maxPoolSize) {
+            try {
+                connection = DriverManager.getConnection(dbUrl, dbUser, dbPass);
+                connectionQueue.put(connection);
+            } catch (InterruptedException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public synchronized Connection takeConnection() {
+        Connection connection = null;
+        try {
+            connection = connectionQueue.take();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public synchronized void returnConnection(Connection connection) {
+        if ((connection != null) && (connectionQueue.size() <= maxPoolSize)) {
+            try {
+                connectionQueue.put(connection);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
